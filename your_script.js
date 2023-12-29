9397807498633a60054edfa7596b4c31cc1a5456
sisminnmaw <sisminnmaw@gmail.com>
2023-12-29 18:22:13 +0900

    updated guide and volunteer

--- your_script.js
+++ your_script.js
@@ -6,6 +6,20 @@ function updateText() {
     document.getElementById('displayIssue').textContent = document.getElementById('issue').value;
 }
 
+function updateGuideText() {
+    document.getElementById('displayGuideName').textContent = document.getElementById('guideName').value;
+    document.getElementById('displayGuideNameSmall').textContent = document.getElementById('guideName').value;
+    document.getElementById('displaySession').textContent = document.getElementById('session').value;
+    document.getElementById('displayDate').textContent = document.getElementById('date').value;
+    document.getElementById('displayIssue').textContent = document.getElementById('issue').value;
+}
+
+function updateVolunteerText() {
+    document.getElementById('displayVolunteerName').textContent = document.getElementById('volunteerName').value;
+    document.getElementById('displaySession').textContent = document.getElementById('session').value;
+    document.getElementById('displayDate').textContent = document.getElementById('date').value;
+    document.getElementById('displayIssue').textContent = document.getElementById('issue').value;
+}
 function downloadPNG() {
     html2canvas(document.getElementById("content"),{ useCORS: true }).then(canvas => {
         var imageURL = canvas.toDataURL("image/png");
